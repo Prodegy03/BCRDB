cd8a78e6559b3f5b618d5d82315b0a0a05e58f87
Prodegy03 <chri17052@byui.edu>
2023-04-01 11:26:37 -0600

    dejunk

--- brcdb.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `users` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `fullname` varchar(255) NOT NULL,
- `email` varchar(255) NOT NULL,
- `password` varchar(255) NOT NULL,
- `activation_key` varchar(255) DEFAULT NULL,
- `status` int(11) NOT NULL DEFAULT 0 COMMENT '1=Active|0=Inactive',
- PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
