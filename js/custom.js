2246cc3b52a204f950c8eb0eb383167aa6499bcb
Prodegy03 <chri17052@byui.edu>
2023-04-04 14:48:45 -0600

    submit

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,4 @@
-/* Author:WebThemez
- * Author URI:http://webthemez.com
- * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
- */
+
 
 (function($){
 	$(document).ready(function(){
@@ -109,5 +106,6 @@
 			});
 		}
 
-	}); // End document ready
+	}); 
+	// End document ready
 })(this.jQuery);
